ab1267529baaf777b7ee98751998164a438c1788
Julia Guerreiro <juguerreiro123@gmail.com>
2022-02-23 16:29:53 -0300

    already do the restaurant controller and the reviews controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+delirio_tropical = Restaurant.create!({ name: 'Delirio Tropical', address: 'Rua Garcia davila', category: 'french' })
+
+sunomono = Restaurant.create!({ name: 'Sunomono', address: 'Rua siqueira Campos', category: 'japanese' })
+
+spoleto = Restaurant.create!({ name: 'Spoleto', address: 'Rua figueiredo magalhaes', category: 'italian' })
+
+china_in_box = Restaurant.create!({ name: 'China in box', address: 'Rua praia de botafogo', category: 'chinese' })
